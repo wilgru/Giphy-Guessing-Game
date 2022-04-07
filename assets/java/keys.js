5374f27ceb55d7d0202ed8f80a9556adc009cbee
Nicol Mouton <nicolmouton@yahoo.com>
2022-04-07 17:46:08 +1000

    added the api scramble to the keys.js file, removed key.js from gitignore

---
+++ assets/java/keys.js
@@ -0,0 +1,16 @@
+var WordsAPIOneOfThree = "19b86261c7msheae";
+
+var WordsAPITwoOfThree = "50a00b3ebf7cp14c3e6";
+
+var WordsAPIThreeOfThree = "jsn10112d0a7231";
+
+var giphyAPIOneOfThree = "DrJYJBwC72NA";
+
+var giphyAPITwoOfThree = "oBTMVtMicog";
+
+var giphyAPIThreeOfThree = "QEKk5DX6z";
+
+const API_KEYS = {
+  WORDS_API: WordsAPIOneOfThree + WordsAPITwoOfThree + WordsAPIThreeOfThree,
+  GIPHY_API: giphyAPIOneOfThree + giphyAPITwoOfThree + giphyAPIThreeOfThree,
+};
